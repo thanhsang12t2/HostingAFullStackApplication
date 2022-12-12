735ad931ea9f271eda8f060ffb383fce12229a28
duyvien <phan.duy.vien.151@gmail.com>
2022-12-12 11:34:11 -0500

    Update set_env.sh

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=15011993
-export POSTGRES_HOST=database-1.ccbyuqu00sip.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-1.cmbtds0uf7ou.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket781121
+export AWS_BUCKET=arn:aws:s3:::sangtt2
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://udagram-api-dev781121.us-east-1.elasticbeanstalk.com/
+export URL=udagram-api-dev123.us-east-1.elasticbeanstalk.com
